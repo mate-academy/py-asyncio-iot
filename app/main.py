c5600cb59ede8cd5fb196425a80a4ac79bf79292
Yaroslav Demchenko <dastex1411@gmail.com>
2022-12-20 12:20:33 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -19,20 +19,6 @@ async def main() -> None:
     speaker_id = await service.register_device(speaker)
     toilet_id = await service.register_device(toilet)
 
-    # create a few programs
-    wake_up_program = [
-        Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
-    ]
-
-    sleep_program = [
-        Message(hue_light_id, MessageType.SWITCH_OFF),
-        Message(speaker_id, MessageType.SWITCH_OFF),
-        Message(toilet_id, MessageType.FLUSH),
-        Message(toilet_id, MessageType.CLEAN),
-    ]
-
     async def run_sequence(*functions: Awaitable[Any]) -> None:
         for function in functions:
             await function
@@ -41,8 +27,28 @@ async def main() -> None:
         await asyncio.gather(*functions)
 
     # run the programs
-    await run_sequence(service.run_program(wake_up_program))
-    await run_parallel(service.run_program(sleep_program))
+    await run_sequence(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+        run_parallel(
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
+            service.send_msg(
+                Message(
+                    speaker_id,
+                    MessageType.PLAY_SONG,
+                    "Rick Astley - Never Gonna Give You Up",
+                )
+            ),
+        ),
+    )
+
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+        run_sequence(
+            service.send_msg(Message(toilet_id, MessageType.FLUSH)),
+            service.send_msg(Message(toilet_id, MessageType.CLEAN)),
+        ),
+    )
 
 
 if __name__ == "__main__":
