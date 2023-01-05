0d35d64dd7a5660ab0800a9a3115a78ef7fea28e
Dmytro Svirsa <104418771+DmytroSvirsa@users.noreply.github.com>
2023-01-05 15:15:36 +0300

    solution

--- app/iot/devices.py
+++ app/iot/devices.py
@@ -1,64 +1,42 @@
-import time
+import asyncio
 
 from .message import MessageType
 
 
-TIME_TO_SLEEP = 0.5
+class HomeDevice:
+    TIME_TO_SLEEP = 0.5
+    DEVICE_NAME = None
 
+    async def connect(self) -> None:
+        print(f"Connecting {self.DEVICE_NAME}.")
+        await asyncio.sleep(self.TIME_TO_SLEEP)
+        print(f"{self.DEVICE_NAME} connected.")
 
-# of course this code looks dumb, but imagine some real implementations of each method here
-class HueLightDevice:
-    def connect(self) -> None:
-        print("Connecting Hue Light.")
-        time.sleep(TIME_TO_SLEEP)
-        print("Hue Light connected.")
+    async def disconnect(self) -> None:
+        print(f"Disconnecting {self.DEVICE_NAME}.")
+        await asyncio.sleep(self.TIME_TO_SLEEP)
+        print(f"{self.DEVICE_NAME} disconnected.")
 
-    def disconnect(self) -> None:
-        print("Disconnecting Hue Light.")
-        time.sleep(TIME_TO_SLEEP)
-        print("Hue Light disconnected.")
-
-    def send_message(self, message_type: MessageType, data: str = "") -> None:
+    async def send_message(
+            self,
+            message_type: MessageType,
+            data: str = ""
+    ) -> None:
         print(
-            f"Hue Light handling message of type {message_type.name} with data [{data}]."
+            f"{self.DEVICE_NAME} message of type "
+            f"{message_type.name} with data [{data}]."
         )
-        time.sleep(TIME_TO_SLEEP)
-        print("Hue Light received message.")
+        await asyncio.sleep(self.TIME_TO_SLEEP)
+        print(f"{self.DEVICE_NAME} received message.")
 
 
-class SmartSpeakerDevice:
-    def connect(self) -> None:
-        print("Connecting to Smart Speaker.")
-        time.sleep(TIME_TO_SLEEP)
-        print("Smart Speaker connected.")
-
-    def disconnect(self) -> None:
-        print("Disconnecting Smart Speaker.")
-        time.sleep(TIME_TO_SLEEP)
-        print("Smart Speaker disconnected.")
-
-    def send_message(self, message_type: MessageType, data: str = "") -> None:
-        print(
-            f"Smart Speaker handling message of type {message_type.name} with data [{data}]."
-        )
-        time.sleep(TIME_TO_SLEEP)
-        print("Smart Speaker received message.")
+class HueLightDevice(HomeDevice):
+    DEVICE_NAME = "Hue Light"
 
 
-class SmartToiletDevice:
-    def connect(self) -> None:
-        print("Connecting to Smart Toilet.")
-        time.sleep(TIME_TO_SLEEP)
-        print("Smart Toilet connected.")
+class SmartSpeakerDevice(HomeDevice):
+    DEVICE_NAME = "Speaker"
 
-    def disconnect(self) -> None:
-        print("Disconnecting Smart Toilet.")
-        time.sleep(TIME_TO_SLEEP)
-        print("Smart Toilet disconnected.")
 
-    def send_message(self, message_type: MessageType, data: str = "") -> None:
-        print(
-            f"Smart Toilet handling message of type {message_type.name} with data [{data}]."
-        )
-        time.sleep(TIME_TO_SLEEP)
-        print("Smart Toilet received message.")
+class SmartToiletDevice(HomeDevice):
+    DEVICE_NAME = "Toilet"
--- app/iot/service.py
+++ app/iot/service.py
@@ -1,8 +1,11 @@
 import random
 import string
+import asyncio
 from typing import Protocol
 
 from .message import Message, MessageType
+from typing import Awaitable, Any
+
 
 
 def generate_id(length: int = 8) -> str:
@@ -12,13 +15,13 @@ def generate_id(length: int = 8) -> str:
 # Protocol is very similar to ABC, but uses duck typing
 # so devices should not inherit for it (if it walks like a duck, and quacks like a duck, it's a duck)
 class Device(Protocol):
-    def connect(self) -> None:
+    async def connect(self) -> None:
         ...  # Ellipsis - similar to "pass", but sometimes has different meaning
 
-    def disconnect(self) -> None:
+    async def disconnect(self) -> None:
         ...
 
-    def send_message(self, message_type: MessageType, data: str) -> None:
+    async def send_message(self, message_type: MessageType, data: str) -> None:
         ...
 
 
@@ -26,24 +29,39 @@ class IOTService:
     def __init__(self):
         self.devices: dict[str, Device] = {}
 
-    def register_device(self, device: Device) -> str:
-        device.connect()
+    async def register_device(self, device: Device) -> str:
+        await device.connect()
         device_id = generate_id()
         self.devices[device_id] = device
         return device_id
 
-    def unregister_device(self, device_id: str) -> None:
-        self.devices[device_id].disconnect()
+    async def unregister_device(self, device_id: str) -> None:
+        await self.devices[device_id].disconnect()
         del self.devices[device_id]
 
     def get_device(self, device_id: str) -> Device:
         return self.devices[device_id]
 
-    def run_program(self, program: list[Message]) -> None:
+    async def run_program(self, program: list[Message]) -> None:
         print("=====RUNNING PROGRAM======")
+
+        separated_program = {msg.device_id: [] for msg in program}
         for msg in program:
-            self.send_msg(msg)
+            separated_program[msg.device_id].append(msg)
+
+        coroutines = [
+            self.run_sequence(*messages)
+            for messages in separated_program.values()
+        ]
+        await asyncio.gather(*coroutines)
+
         print("=====END OF PROGRAM======")
 
-    def send_msg(self, msg: Message) -> None:
-        self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
+    async def send_msg(self, msg: Message) -> None:
+        await self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
+
+    async def run_sequence(self, *messages: Message) -> None:
+        for message in messages:
+            await self.send_msg(message)
+
+
