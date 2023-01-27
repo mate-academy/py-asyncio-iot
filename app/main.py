ebac45c622b3917ddaf4853105183772d9bba292
Inna Kushnir <innakushnir63@gmail.com>
2023-01-27 12:50:34 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -38,8 +38,24 @@ async def main() -> None:
         Message(toilet_id, MessageType.CLEAN),
     ]
 
-    await run_sequence(service.run_program(wake_up_program))
-    await run_parallel(service.run_program(sleep_program))
+    await run_sequence(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+        run_parallel(
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
+            service.send_msg(Message(
+                speaker_id,
+                MessageType.PLAY_SONG,
+                "Rick Astley - Never Gonna Give You Up"))
+        )
+    )
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+        run_sequence(
+            service.send_msg(Message(toilet_id, MessageType.FLUSH)),
+            service.send_msg(Message(toilet_id, MessageType.CLEAN))
+        )
+    )
 
 
 if __name__ == "__main__":
