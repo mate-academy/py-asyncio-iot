00ec96f2bf2ee20101b2a8d050b8bb931f71c085
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-04-30 14:20:48 +0300

    final solution

--- app/iot/service.py
+++ app/iot/service.py
@@ -1,4 +1,3 @@
-import asyncio
 import random
 import string
 from typing import Protocol
@@ -10,21 +9,19 @@ def generate_id(length: int = 8) -> str:
     return "".join(random.choices(string.ascii_uppercase, k=length))
 
 
-# Protocol is very similar to ABC, but uses duck typing
-# so devices should not inherit for it (if it walks like a duck, and quacks like a duck, it's a duck)
 class Device(Protocol):
-    def connect(self) -> None:
-        ...  # Ellipsis - similar to "pass", but sometimes has different meaning
+    async def connect(self) -> None:
+        ...
 
     def disconnect(self) -> None:
         ...
 
-    def send_message(self, message_type: MessageType, data: str) -> None:
+    async def send_message(self, message_type: MessageType, data: str) -> None:
         ...
 
 
 class IOTService:
-    def __init__(self):
+    def __init__(self) -> None:
         self.devices: dict[str, Device] = {}
 
     async def register_device(self, device: Device) -> str:
@@ -42,8 +39,8 @@ class IOTService:
 
     async def run_program(self, program: list[Message]) -> None:
         print("=====RUNNING PROGRAM======")
-        messages = [asyncio.create_task(self.send_msg(msg)) for msg in program]
-        await asyncio.gather(*messages)
+        for msg in program:
+            await self.send_msg(msg)
         print("=====END OF PROGRAM======")
 
     async def send_msg(self, msg: Message) -> None:
--- app/main.py
+++ app/main.py
@@ -2,15 +2,9 @@ import asyncio
 import time
 from typing import Any, Awaitable
 
-
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
 from iot.service import IOTService
-
-
-async def device_connection(service, device):
-    device_id = await service.register_device(device)
-    return device_id
 
 
 async def run_sequence(*functions: Awaitable[Any]) -> None:
@@ -23,37 +17,40 @@ async def run_parallel(*functions: Awaitable[Any]) -> None:
 
 
 async def main() -> None:
-    # create an IOT service
     service = IOTService()
 
-    # create and register a few devices
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
 
-    hue_light_id, speaker_id, toilet_id = await asyncio.gather(device_connection(service,hue_light), device_connection(service, speaker), device_connection(service,toilet))
+    hue_light_id, speaker_id, toilet_id = await asyncio.gather(
+        service.register_device(hue_light),
+        service.register_device(speaker),
+        service.register_device(toilet),
+    )
 
-    # create a few programs
-    # wake_up_program = [
-    #     Message(hue_light_id, MessageType.SWITCH_ON),
-    #     Message(speaker_id, MessageType.SWITCH_ON),
-    #     Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
-    #
-    # ]
-    #
-    # sleep_program = [
-    #     Message(hue_light_id, MessageType.SWITCH_OFF),
-    #     Message(speaker_id, MessageType.SWITCH_OFF),
-    #     Message(toilet_id, MessageType.FLUSH),
-    #     Message(toilet_id, MessageType.CLEAN),
-    # ]
+    await run_sequence(
+        run_parallel(
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
+        ),
+        service.send_msg(
+            Message(
+                speaker_id,
+                MessageType.PLAY_SONG,
+                "Rick Astley - Never Gonna Give You Up",
+            )
+        ),
+    )
 
-    wake_up_program = run_parallel()
-    #
-    # # run the programs
-    await asyncio.gather(service.run_program(wake_up_program))
-    # await asyncio.gather(service.run_program(sleep_program))
-    # service.run_program(sleep_program)
+    await run_sequence(
+        run_parallel(
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+            service.send_msg((Message(toilet_id, MessageType.FLUSH))),
+        ),
+        service.send_msg((Message(toilet_id, MessageType.CLEAN))),
+    )
 
 
 if __name__ == "__main__":
