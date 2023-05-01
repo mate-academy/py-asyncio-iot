c7b2d33a96b59d40632a63fa78e647993d6ad760
Anatolii Pozniak <a13101990a@gmail.com>
2023-05-01 13:09:09 +0300

    task 2

--- app/main.py
+++ app/main.py
@@ -14,9 +14,14 @@ async def main() -> None:
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    hue_light_id = await service.register_device(hue_light)
-    speaker_id = await service.register_device(speaker)
-    toilet_id = await service.register_device(toilet)
+    # hue_light_id = await service.register_device(hue_light)
+    # speaker_id = await service.register_device(speaker)
+    # toilet_id = await service.register_device(toilet)
+    hue_light_id, speaker_id, toilet_id = await asyncio.gather(
+        service.register_device(hue_light),
+        service.register_device(speaker),
+        service.register_device(toilet)
+    )
 
     # create a few programs
     wake_up_program = [
