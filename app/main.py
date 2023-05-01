9817bc60834e148c2788bc6a70ba8697f07abc0f
Anatolii Pozniak <a13101990a@gmail.com>
2023-05-01 13:17:20 +0300

    task 3.1 - total destruction. MAX SPEED

--- app/main.py
+++ app/main.py
@@ -38,8 +38,8 @@ async def main() -> None:
     ]
 
     # run the programs
-    await service.run_program(wake_up_program)
-    await service.run_program(sleep_program)
+    await asyncio.gather(service.run_program(wake_up_program), service.run_program(sleep_program))
+    # await service.run_program(sleep_program)
 
 
 if __name__ == "__main__":
