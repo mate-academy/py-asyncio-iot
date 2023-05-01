e971c48207f91f09d6bce52190b1185f44f6b33a
Zhukov_Oleksandr <zhukov9523@gmail.com>
2023-05-01 22:50:49 +0300

    Changed sequence

--- app/main.py
+++ app/main.py
@@ -7,18 +7,18 @@ from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
+async def run_sequence(*functions: Awaitable[Any]) -> None:
+    for function in functions:
+        await function
+
+
+async def run_parallel(*functions: Awaitable[Any]) -> None:
+    await asyncio.gather(*functions)
+
+
 async def main() -> None:
-    async def run_sequence(*functions: Awaitable[Any]) -> None:
-        for function in functions:
-            await function
-
-    async def run_parallel(*functions: Awaitable[Any]) -> None:
-        await asyncio.gather(*functions)
-
-    # create an IOT service
     service = IOTService()
 
-    # create and register a few devices
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
@@ -28,16 +28,12 @@ async def main() -> None:
         service.register_device(toilet)
     )
 
-    # create a few programs
     await run_sequence(
         run_parallel(
             service.send_msg(Message(device_ids[0], MessageType.SWITCH_ON)),
             service.send_msg(Message(device_ids[1], MessageType.SWITCH_ON)),
         ),
         service.send_msg(Message(device_ids[1], MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up")),
-    )
-
-    await run_sequence(
         run_parallel(
             service.send_msg(Message(device_ids[0], MessageType.SWITCH_OFF)),
             service.send_msg(Message(device_ids[1], MessageType.SWITCH_OFF)),
@@ -45,8 +41,6 @@ async def main() -> None:
         ),
         service.send_msg(Message(device_ids[2], MessageType.CLEAN))
     )
-
-    # run the programs
 
 if __name__ == "__main__":
     start = time.perf_counter()
