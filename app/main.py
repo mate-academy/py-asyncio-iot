2c86a9dd415f27f13034ffeeeec8ce0b8e775c71
Viktoria Rybenchuk <rybenchukv@gmail.com>
2023-05-10 17:26:05 +0200

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -36,33 +36,34 @@ async def main() -> None:
 
     hue_light_id, speaker_id, toilet_id = ids
 
-    switch_on = Message(hue_light_id, MessageType.SWITCH_ON)
-    speaker_switch_on = Message(speaker_id, MessageType.SWITCH_ON)
-    play_song = Message(
-        speaker_id,
-        MessageType.PLAY_SONG,
-        "Rick Astley - Never Gonna Give You Up"
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_ON))
     )
-
-    await run_parallel(service.send_msg
-                       (switch_on),
-                       service.send_msg(speaker_switch_on)
-                       )
-    await run_sequence(service.send_msg(play_song))
+    await run_sequence(
+        service.send_msg(
+            Message(
+                speaker_id,
+                MessageType.PLAY_SONG,
+                "Rick Astley - Never Gonna Give You Up"
+            )
+        )
+    )
     await run_parallel(
         service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
         service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
         service.send_msg(Message(toilet_id, MessageType.FLUSH))
 
     )
-    await run_sequence(service.send_msg(Message(
-        toilet_id, MessageType.CLEAN))
+    await run_sequence(
+        service.send_msg(Message(toilet_id, MessageType.CLEAN))
     )
     await run_parallel(
         service.unregister_device(hue_light_id),
         service.unregister_device(speaker_id),
         service.unregister_device(toilet_id)
     )
+
 
 if __name__ == "__main__":
     start = time.perf_counter()
