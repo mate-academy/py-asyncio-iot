0e0ae3815386607ca2050c4f14688713724d8131
Sergii Machulin <machulinsv@gmail.com>
2023-05-10 18:47:24 +0300

    Fix solution

--- app/main.py
+++ app/main.py
@@ -31,37 +31,29 @@ async def main() -> None:
         service.register_device(toilet),
     )
 
-    # run the programs
     await run_sequence(
-        service.run_program(
-            [
-                Message(hue_light_id, MessageType.SWITCH_ON),
-                Message(speaker_id, MessageType.SWITCH_ON),
-            ]
+        run_parallel(
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
         ),
-        service.run_program(
-            [
+        run_sequence(
+            service.send_msg(
                 Message(
                     speaker_id,
                     MessageType.PLAY_SONG,
                     "Rick Astley - Never Gonna Give You Up",
-                ),
-                Message(toilet_id, MessageType.FLUSH),
-            ]
-        ),
-        service.run_program(
-            [
-                Message(hue_light_id, MessageType.SWITCH_OFF),
-                Message(speaker_id, MessageType.SWITCH_OFF),
-                Message(toilet_id, MessageType.CLEAN),
-            ]
+                )
+            )
         ),
     )
 
-    await asyncio.gather(
-        service.unregister_device(hue_light_id),
-        service.unregister_device(speaker_id),
-        service.unregister_device(toilet_id),
+    await run_sequence(
+        run_parallel(
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+            service.send_msg((Message(toilet_id, MessageType.FLUSH))),
+        ),
+        run_sequence(service.send_msg((Message(toilet_id, MessageType.CLEAN)))),
     )
 
 
