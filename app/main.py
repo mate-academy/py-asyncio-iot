b92d6170645ecace3c48619d2d742eb2a5622cd2
Іван Березянський <mamanadarin@gmail.com>
2023-05-19 08:43:54 +0300

    flake8

--- app/main.py
+++ app/main.py
@@ -35,19 +35,30 @@ async def main() -> None:
 
     # create a few programs
     await run_parallel(
-        service.run_program([Message(hue_light_id, MessageType.SWITCH_ON)]),
-        run_sequence(service.run_program([Message(speaker_id, MessageType.SWITCH_ON)]),
-                     service.run_program([Message(speaker_id, MessageType.PLAY_SONG,
-                                                  "Rick Astley - Never Gonna Give You Up")])
-                     )
+        service.run_program(
+            [Message(hue_light_id,
+                     MessageType.SWITCH_ON)]
+        ),
+        run_sequence(service.run_program(
+            [Message(speaker_id,
+                     MessageType.SWITCH_ON)]
+        ),
+            service.run_program(
+                [Message(speaker_id,
+                         MessageType.PLAY_SONG,
+                         "Rick Astley - Never Gonna Give You Up")]
+        ), )
     )
 
     await run_parallel(
         service.run_program([Message(hue_light_id, MessageType.SWITCH_OFF)]),
         service.run_program([Message(speaker_id, MessageType.SWITCH_OFF)]),
-        run_sequence(service.run_program([Message(toilet_id, MessageType.FLUSH)]),
-                     service.run_program([Message(toilet_id, MessageType.CLEAN)]),
-                     ))
+        run_sequence(service.run_program([Message(toilet_id,
+                                                  MessageType.FLUSH)]),
+                     service.run_program([Message(toilet_id,
+                                                  MessageType.CLEAN)]),
+                     )
+    )
 
 
 if __name__ == "__main__":
