4bab1f6eb048a825d2087982c6e7fac79c8dbed9
Anastasiia <nastyashch1601@gmail.com>
2023-07-13 17:12:02 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -34,25 +34,25 @@ async def main() -> None:
     await run_sequence(
         run_parallel(
             service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
-            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
-            service.send_msg(
-                Message(
-                    speaker_id,
-                    MessageType.PLAY_SONG,
-                    "Rick Astley - Never Gonna Give You Up"
-                )
-            ),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON))
+        ),
+        service.send_msg(
+            Message(
+                speaker_id,
+                MessageType.PLAY_SONG,
+                "Rick Astley - Never Gonna Give You Up"
             )
-        )
+        ),
+    )
 
     await run_sequence(
         run_parallel(
             service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
-            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
-            service.send_msg(Message(toilet_id, MessageType.FLUSH)),
-            service.send_msg(Message(toilet_id, MessageType.CLEAN)),
-            )
-        )
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF))
+        ),
+        service.send_msg(Message(toilet_id, MessageType.FLUSH)),
+        service.send_msg(Message(toilet_id, MessageType.CLEAN)),
+    )
 
 
 if __name__ == "__main__":
