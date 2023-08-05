5475eed3ed6e6cd3ddba31541929740cf427c5ba
Ivan <ivan.korshunov.1997@gmail.com>
2023-08-05 19:52:26 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
@@ -5,7 +6,7 @@ from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
-def main() -> None:
+async def main() -> None:
     # create an IOT service
     service = IOTService()
 
@@ -13,9 +14,13 @@ def main() -> None:
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    hue_light_id = service.register_device(hue_light)
-    speaker_id = service.register_device(speaker)
-    toilet_id = service.register_device(toilet)
+
+    task1 = asyncio.create_task(service.register_device(hue_light))
+    task2 = asyncio.create_task(service.register_device(speaker))
+    task3 = asyncio.create_task(service.register_device(toilet))
+
+    list_of_id = await asyncio.gather(task1, task2, task3)
+    hue_light_id, speaker_id, toilet_id = list_of_id
 
     # create a few programs
     wake_up_program = [
@@ -32,13 +37,13 @@ def main() -> None:
     ]
 
     # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    await service.run_program(wake_up_program)
+    await service.run_program(sleep_program)
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+    asyncio.run(main())
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
