27feff49b3ef0030dd60853985d3585efc1a8e37
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-08-08 18:54:31 +0300

    Fix style

--- app/main.py
+++ app/main.py
@@ -24,37 +24,22 @@ async def main() -> None:
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    # hue_light_id = await service.register_device(hue_light)
-    # speaker_id = await service.register_device(speaker)
-    # toilet_id = await service.register_device(toilet)
     hue_light_id, speaker_id, toilet_id = await asyncio.gather(
-        *[service.register_device(device) for device in (hue_light, speaker, toilet)]
+        *[
+            service.register_device(device)
+            for device in (hue_light, speaker, toilet)
+        ]
     )
-
-    # # create a few programs
-    # wake_up_program = [
-    #     Message(hue_light_id, MessageType.SWITCH_ON),
-    #     Message(speaker_id, MessageType.SWITCH_ON),
-    #     Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
-    # ]
-
-    # sleep_program = [
-    #     Message(hue_light_id, MessageType.SWITCH_OFF),
-    #     Message(speaker_id, MessageType.SWITCH_OFF),
-    #     Message(toilet_id, MessageType.FLUSH),
-    #     Message(toilet_id, MessageType.CLEAN),
-    # ]
-
-    # # run the programs
-    # await service.run_program(wake_up_program)
-    # await service.run_program(sleep_program)
 
     await run_parallel(service.run_program([
         Message(hue_light_id, MessageType.SWITCH_ON),
         Message(speaker_id, MessageType.SWITCH_ON),
     ]))
     await run_sequence(service.run_program([
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up")
+        Message(
+            speaker_id,
+            MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"
+        )
     ]))
 
     await run_parallel(service.run_program([
