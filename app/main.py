7c5462ca84fb4d77ccf08d6ed6924b5492ddf21f
neostyle88 <makssimm1@gmail.com>
2023-08-10 18:59:06 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -30,12 +30,16 @@ async def main() -> None:
         service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
         service.send_msg(Message(speaker_id, MessageType.PLAY_SONG,
                                  "Rick Astley - Never Gonna Give You Up")),
+        ]
+    )
+
+    await service.run_program([
         service.send_msg(
             Message(hue_light_id, MessageType.SWITCH_OFF)),
         service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
         service.send_msg(Message(toilet_id, MessageType.FLUSH)),
         service.send_msg(Message(toilet_id, MessageType.CLEAN)),
-        ]
+    ]
     )
 
 
