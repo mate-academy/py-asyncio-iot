e81a6848020d3a10f68c02b7ab8fa96c5cb8cb8d
Olha Tryhub <olha.tryhub@nure.ua>
2023-08-22 10:32:01 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import time
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
@@ -5,40 +6,51 @@ from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
-def main() -> None:
+async def register_devices(service, devices):
+    ids = await asyncio.gather(
+        *[service.register_device(device) for device in devices]
+    )
+    return ids
+
+
+async def run_programs(service, programs):
+    await asyncio.gather(
+        *[service.run_program(program) for program in programs]
+    )
+
+
+async def async_main() -> None:
     # create an IOT service
     service = IOTService()
 
     # create and register a few devices
-    hue_light = HueLightDevice()
-    speaker = SmartSpeakerDevice()
-    toilet = SmartToiletDevice()
-    hue_light_id = service.register_device(hue_light)
-    speaker_id = service.register_device(speaker)
-    toilet_id = service.register_device(toilet)
+    devices = [HueLightDevice(), SmartSpeakerDevice(), SmartToiletDevice()]
+
+    device_ids = await register_devices(service, devices)
 
     # create a few programs
     wake_up_program = [
-        Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
+        Message(device_ids[0], MessageType.SWITCH_ON),
+        Message(device_ids[1], MessageType.SWITCH_ON),
+        Message(device_ids[1], MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
     ]
 
     sleep_program = [
-        Message(hue_light_id, MessageType.SWITCH_OFF),
-        Message(speaker_id, MessageType.SWITCH_OFF),
-        Message(toilet_id, MessageType.FLUSH),
-        Message(toilet_id, MessageType.CLEAN),
+        Message(device_ids[0], MessageType.SWITCH_OFF),
+        Message(device_ids[1], MessageType.SWITCH_OFF),
+        Message(device_ids[2], MessageType.FLUSH),
+        Message(device_ids[2], MessageType.CLEAN),
     ]
 
     # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    await run_programs(service, [wake_up_program, sleep_program])
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+
+    asyncio.run(async_main())
+
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
