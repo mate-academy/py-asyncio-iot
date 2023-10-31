2fbec4517571b17e70fcf9a0f945d9c53dabc98c
anna-lybid <anna.lybid@gmail.com>
2023-10-31 15:18:07 +0200

    Small refactoring

--- app/main.py
+++ app/main.py
@@ -21,13 +21,10 @@ async def main() -> None:
     service = IOTService()
 
     # create and register a few devices
-    hue_light = HueLightDevice()
-    speaker = SmartSpeakerDevice()
-    toilet = SmartToiletDevice()
     hue_light_id, speaker_id, toilet_id = await asyncio.gather(
-        service.register_device(hue_light),
-        service.register_device(speaker),
-        service.register_device(toilet),
+        service.register_device(HueLightDevice()),
+        service.register_device(SmartSpeakerDevice()),
+        service.register_device(SmartToiletDevice()),
     )
 
     await run_parallel(
