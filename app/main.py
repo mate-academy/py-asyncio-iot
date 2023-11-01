d9398d6c9e62af984dd352cc14fd4b9f21160b42
Volodymyr Bohomol <8773927@gmail.com>
2023-11-01 15:02:27 +0200

    Fix #1

--- app/main.py
+++ app/main.py
@@ -34,28 +34,24 @@ async def main() -> None:
     hue_light_id, speaker_id, toilet_id = await asyncio.gather(*tasks)
 
     await run_parallel(
-        service.run_program(
-            [
-                Message(hue_light_id, MessageType.SWITCH_ON),
-                Message(speaker_id, MessageType.SWITCH_ON),
-                Message(
-                    speaker_id,
-                    MessageType.PLAY_SONG,
-                    "Rick Astley - Never Gonna Give You Up",
-                ),
-            ]
-        )
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
+        service.send_msg(
+            Message(
+                speaker_id,
+                MessageType.PLAY_SONG,
+                "Rick Astley - Never Gonna Give You Up",
+            ),
+        ),
     )
 
     await run_sequence(
-        service.run_program(
-            [
-                Message(hue_light_id, MessageType.SWITCH_OFF),
-                Message(speaker_id, MessageType.SWITCH_OFF),
-                Message(toilet_id, MessageType.FLUSH),
-                Message(toilet_id, MessageType.CLEAN),
-            ]
-        )
+        run_parallel(
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+            service.send_msg(Message(toilet_id, MessageType.FLUSH)),
+        ),
+        service.send_msg(Message(toilet_id, MessageType.CLEAN)),
     )
 
 
