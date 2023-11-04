bed26b4709f1914af256fea5698b9df2cd28e93c
eduardhabryd <eduardhabryd@gmail.com>
2023-11-04 09:57:56 +0200

    changed logic

--- app/main.py
+++ app/main.py
@@ -24,46 +24,56 @@ async def main() -> None:
     async def run_parallel(*functions: Awaitable[Any]) -> None:
         await asyncio.gather(*functions)
 
-    await run_parallel(
-        service.run_program(
-            [
+    await run_sequence(
+        run_parallel(
+            service.send_msg(
                 Message(
                     hue_light_id,
                     MessageType.SWITCH_ON,
-                ),
+                )
+            ),
+            service.send_msg(
                 Message(
                     speaker_id,
                     MessageType.SWITCH_ON,
-                ),
-                Message(
-                    speaker_id,
-                    MessageType.PLAY_SONG,
-                    "Rick Astley - Never Gonna Give You Up",
-                ),
-            ]
+                )
+            )
+        ),
+        service.send_msg(
+            Message(
+                speaker_id,
+                MessageType.PLAY_SONG,
+                "Rick Astley - Never Gonna Give You Up",
+            ),
         )
-    ),
+    )
 
     await run_sequence(
-        service.run_program(
-            [
+        run_parallel(
+            service.send_msg(
                 Message(
                     hue_light_id,
                     MessageType.SWITCH_OFF,
                 ),
+            ),
+            service.send_msg(
                 Message(
                     speaker_id,
                     MessageType.SWITCH_OFF,
                 ),
-                Message(
-                    toilet_id,
-                    MessageType.FLUSH,
-                ),
-                Message(
-                    toilet_id,
-                    MessageType.CLEAN,
-                ),
-            ]
+            )
+        ),
+        service.send_msg(
+            Message(
+                toilet_id,
+                MessageType.FLUSH,
+            ),
+        ),
+        service.send_msg(
+            Message(
+                toilet_id,
+                MessageType.CLEAN,
+            ),
         )
     )
 
