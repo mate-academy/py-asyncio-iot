afc3681e1a1c464a9015ccb2174c85c1735bf9b8
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-11-07 16:07:08 +0200

    py-asynco-iot

--- app/main.py
+++ app/main.py
@@ -31,15 +31,16 @@ async def main() -> None:
 
     # run the programs
     await run_sequence(
-        service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
         run_parallel(
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
             service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
-            service.send_msg(Message(speaker_id,
-                                     MessageType.PLAY_SONG,
-                                     "Rick Astley - Never Gonna Give You Up"
-                                     )
-                             ),
-        )
+        ),
+        service.send_msg(Message(speaker_id,
+                                 MessageType.PLAY_SONG,
+                                 "Rick Astley - Never Gonna Give You Up"
+                                 )
+
+                         )
     )
     await run_parallel(
         service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
