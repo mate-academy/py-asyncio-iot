dab111b739f64c544891c959554b1de9de804e70
Pavlo Harbuz <lgsilay1991@gmail.com>
2023-11-08 18:39:30 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -29,34 +29,35 @@ async def main() -> None:
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
 
-    devices = (hue_light, speaker, toilet,)
-
-    registered_devices = await asyncio.gather(
-        *(service.register_device(device) for device in devices)
-    )
-    hue_light_id, speaker_id, toilet_id = registered_devices
-
     # create a few programs
-    wake_up_program = [
-        Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
-        Message(
-            speaker_id,
-            MessageType.PLAY_SONG,
-            "Rick Astley - Never Gonna Give You Up"
-        ),
+    tasks = [
+        service.register_device(hue_light),
+        service.register_device(speaker),
+        service.register_device(toilet),
     ]
 
-    sleep_program = [
-        Message(hue_light_id, MessageType.SWITCH_OFF),
-        Message(speaker_id, MessageType.SWITCH_OFF),
-        Message(toilet_id, MessageType.FLUSH),
-        Message(toilet_id, MessageType.CLEAN),
-    ]
+    hue_light_id, speaker_id, toilet_id = await asyncio.gather(*tasks)
 
     # run the programs
-    await run_parallel(service.run_program(wake_up_program))
-    await run_sequence(service.run_program(sleep_program))
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_ON))
+    )
+    await service.send_msg(Message(
+        speaker_id,
+        MessageType.PLAY_SONG,
+        "Rick Astley - Never Gonna Give You Up"
+    )),
+
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+    )
+
+    await run_sequence(
+        service.send_msg(Message(toilet_id, MessageType.FLUSH)),
+        service.send_msg(Message(toilet_id, MessageType.CLEAN))
+    )
 
 
 if __name__ == "__main__":
