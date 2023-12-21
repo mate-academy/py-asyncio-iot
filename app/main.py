7abe239cacd77f33efcc93f0a633077840ae2e87
Kitaiskyi Oleksii <leshafamilenko@gmail.com>
2023-12-21 16:05:14 +0200

    sol

--- app/main.py
+++ app/main.py
@@ -1,11 +1,21 @@
 import time
-
+import asyncio
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
-from iot.message import Message, MessageType
+from iot.message import MessageType
 from iot.service import IOTService
+from typing import Any, Awaitable
 
 
-def main() -> None:
+async def run_sequence(*functions: Awaitable[Any]) -> None:
+    for function in functions:
+        await function
+
+
+async def run_parallel(*functions: Awaitable[Any]) -> None:
+    await asyncio.gather(*functions)
+
+
+async def main() -> None:
     # create an IOT service
     service = IOTService()
 
@@ -13,32 +23,40 @@ def main() -> None:
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    hue_light_id = service.register_device(hue_light)
-    speaker_id = service.register_device(speaker)
-    toilet_id = service.register_device(toilet)
+
+    await asyncio.gather(
+        service.register_device(hue_light),
+        service.register_device(speaker),
+        service.register_device(toilet),
+    )
 
     # create a few programs
-    wake_up_program = [
-        Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
-    ]
+    song = "Rick Astley - Never Gonna Give You Up"
 
-    sleep_program = [
-        Message(hue_light_id, MessageType.SWITCH_OFF),
-        Message(speaker_id, MessageType.SWITCH_OFF),
-        Message(toilet_id, MessageType.FLUSH),
-        Message(toilet_id, MessageType.CLEAN),
-    ]
+    light_on = hue_light.send_message(MessageType.SWITCH_ON)
+    light_off = hue_light.send_message(MessageType.SWITCH_OFF)
 
-    # run the programs
-    service.run_program(wake_up_program)
-    service.run_program(sleep_program)
+    speaker_on = speaker.send_message(MessageType.SWITCH_ON)
+    speaker_off = speaker.send_message(MessageType.SWITCH_OFF)
+    speaker_play = speaker.send_message(MessageType.PLAY_SONG, song)
+
+    toilet_flush = toilet.send_message(MessageType.FLUSH)
+    toilet_clean = toilet.send_message(MessageType.CLEAN)
+
+    light_and_speaker_on = run_parallel(light_on, speaker_on)
+    toilet_use = run_sequence(toilet_flush, toilet_clean)
+
+    morning = run_sequence(light_and_speaker_on, speaker_play)
+    evening = run_parallel(light_off, speaker_off, toilet_use)
+
+    await morning
+
+    await evening
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
-    main()
+    asyncio.run(main())
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
