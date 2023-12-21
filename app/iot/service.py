1c32839f1b1fc679d0b36d62a43e1e90928f67f1
Lytillis <chemurchem@gmail.com>
2023-12-21 21:04:14 +0200

    Asyncio IOT solution

--- app/iot/service.py
+++ app/iot/service.py
@@ -1,6 +1,7 @@
 import random
 import string
-from typing import Protocol
+import asyncio
+from typing import Protocol, Any, Awaitable
 
 from .message import Message, MessageType
 
@@ -10,10 +11,14 @@ def generate_id(length: int = 8) -> str:
 
 
 # Protocol is very similar to ABC, but uses duck typing
-# so devices should not inherit for it (if it walks like a duck, and quacks like a duck, it's a duck)
+# so devices should not inherit for it (if it walks like
+# a duck, and quacks like a duck, it's a duck)
 class Device(Protocol):
     def connect(self) -> None:
-        ...  # Ellipsis - similar to "pass", but sometimes has different meaning
+        """
+        Ellipsis - similar to "pass", but sometimes has different meaning
+        """
+        ...
 
     def disconnect(self) -> None:
         ...
@@ -26,8 +31,8 @@ class IOTService:
     def __init__(self) -> None:
         self.devices: dict[str, Device] = {}
 
-    def register_device(self, device: Device) -> str:
-        device.connect()
+    async def register_device(self, device: Device) -> str:
+        await device.connect()
         device_id = generate_id()
         self.devices[device_id] = device
         return device_id
@@ -39,11 +44,16 @@ class IOTService:
     def get_device(self, device_id: str) -> Device:
         return self.devices[device_id]
 
-    def run_program(self, program: list[Message]) -> None:
+    async def run_sequence(self, *functions: Awaitable[Any]) -> None:
         print("=====RUNNING PROGRAM======")
-        for msg in program:
-            self.send_msg(msg)
+        for function in functions:
+            await function
         print("=====END OF PROGRAM======")
 
-    def send_msg(self, msg: Message) -> None:
-        self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
+    async def run_parallel(self, program: list[Message]) -> None:
+        await asyncio.gather(
+            *[self.send_msg(msg) for msg in program]
+        )
+
+    async def send_msg(self, msg: Message) -> None:
+        await self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
