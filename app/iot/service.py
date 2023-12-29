b30b2fe70f3d175ede56f183885e7d1818c103e4
Hlova Oleh <hlovaoleh0@gmail.com>
2023-12-29 11:50:09 +0200

    the code has been changed to remove the unpacking of the program variable in the run_program function

--- app/iot/service.py
+++ app/iot/service.py
@@ -42,12 +42,10 @@ class IOTService:
     def get_device(self, device_id: str) -> Device:
         return self.devices[device_id]
 
-    async def run_program(self, *program: Message) -> None:
+    async def run_program(self, program: list[Message]) -> None:
         print("=====RUNNING PROGRAM======")
-        running_programs = asyncio.gather(
-            *[self.send_msg(msg) for msg in program]
-        )
-        await running_programs
+        for msg in program:
+            await self.send_msg(msg)
         print("=====END OF PROGRAM======")
 
     async def send_msg(self, msg: Message) -> None:
--- app/main.py
+++ app/main.py
@@ -7,15 +7,6 @@ from iot.message import Message, MessageType
 from iot.service import IOTService
 
 
-async def run_sequence(*functions: Awaitable[Any]) -> None:
-    for function in functions:
-        await function
-
-
-async def run_parallel(*functions: Awaitable[Any]) -> None:
-    await asyncio.gather(*functions)
-
-
 async def main() -> None:
     # create an IOT service
     service = IOTService()
@@ -31,27 +22,35 @@ async def main() -> None:
         service.register_device(toilet)
     )
 
+    async def run_sequence(*functions: Awaitable[Any]) -> None:
+        for function in functions:
+            await function
+
+    async def run_parallel(*functions: Awaitable[Any]) -> None:
+        await asyncio.gather(*functions)
+
     await run_sequence(
         run_parallel(
-            service.run_program(Message(hue_light_id, MessageType.SWITCH_ON)),
-            service.run_program(Message(speaker_id, MessageType.SWITCH_ON))
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+            run_sequence(
+                service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
+                service.send_msg(
+                    Message(
+                        speaker_id,
+                        MessageType.PLAY_SONG,
+                        "Rick Astley - Never Gonna Give You Up",
+                    )
+                ),
+            ),
         ),
-        service.run_program(
-            Message(
-                speaker_id,
-                MessageType.PLAY_SONG,
-                "Rick Astley - Never Gonna Give You Up"
-            )
+        run_parallel(
+            service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+            run_sequence(
+                service.send_msg(Message(toilet_id, MessageType.FLUSH)),
+                service.send_msg(Message(toilet_id, MessageType.CLEAN)),
+            ),
         ),
-    )
-
-    await run_parallel(
-        service.run_program(Message(hue_light_id, MessageType.SWITCH_OFF)),
-        service.run_program(Message(speaker_id, MessageType.SWITCH_OFF)),
-        run_sequence(
-            service.run_program(Message(toilet_id, MessageType.FLUSH)),
-            service.run_program(Message(toilet_id, MessageType.CLEAN))
-        )
     )
 
 
