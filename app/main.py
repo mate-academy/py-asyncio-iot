1271c6d23645b7d289ef94dd5984b3d851256700
Vlad Larin <lordgadyka71@gmail.com>
2024-01-27 23:51:34 +0200

    Replace wake_up_program & sleep_program with sequences of run_sequence, run_parallel

--- app/main.py
+++ app/main.py
@@ -28,31 +28,30 @@ async def main() -> None:
     hue_light_id = device_ids[0]
     speaker_id = device_ids[1]
     toilet_id = device_ids[2]
-    # toilet_id = await service.register_device(toilet)
 
-    # create a few programs
-    wake_up_program = [
-        Message(hue_light_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
-    ]
+    # run wake up sequence
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_ON)),
+        run_sequence(
+            service.send_msg(Message(speaker_id, MessageType.SWITCH_ON)),
+            service.send_msg(Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up")),
+        )
+    )
 
-    sleep_program = [
-        Message(hue_light_id, MessageType.SWITCH_OFF),
-        Message(speaker_id, MessageType.SWITCH_OFF),
-        Message(toilet_id, MessageType.FLUSH),
-        Message(toilet_id, MessageType.CLEAN),
-    ]
-
-    # run the programs
-    await service.run_program(wake_up_program)
-    await service.run_program(sleep_program)
+    # run sleep sequence
+    await run_parallel(
+        service.send_msg(Message(hue_light_id, MessageType.SWITCH_OFF)),
+        service.send_msg(Message(speaker_id, MessageType.SWITCH_OFF)),
+        run_sequence(
+            service.send_msg(Message(toilet_id, MessageType.FLUSH)),
+            service.send_msg(Message(toilet_id, MessageType.CLEAN)),
+        )
+    )
 
 
 if __name__ == "__main__":
     start = time.perf_counter()
     asyncio.run(main())
-    # main()
     end = time.perf_counter()
 
     print("Elapsed:", end - start)
